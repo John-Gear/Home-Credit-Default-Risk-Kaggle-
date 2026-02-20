9e17a8af29b614700f53405ca4035883d378269c
John-Gear <plotnikoff.sergey@gmail.com>
2026-02-20 20:39:00 +0300

    Update db

---
+++ src/db.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH_TRAIN = BASE_DIR / "data" / "application_train.db"
+DB_PATH_TEST  = BASE_DIR / "data" / "application_test.db"
+
+def read_sql_train(query: str) -> pd.DataFrame:
+    conn = sqlite3.connect(str(DB_PATH_TRAIN))
+    try:
+        df = pd.read_sql_query(query, conn)
+        return df
+    finally:
+        conn.close()
+
+def read_sql_test(query: str) -> pd.DataFrame:
+    conn = sqlite3.connect(str(DB_PATH_TEST))
+    try:
+        df = pd.read_sql_query(query, conn)
+        return df
+    finally:
+        conn.close()
