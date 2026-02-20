619fbeea32790e4d9592b88f54749dff146c1878
John-Gear <plotnikoff.sergey@gmail.com>
2026-02-20 21:05:24 +0300

    build preprocessor

---
+++ src/preprocessor.py
@@ -0,0 +1,61 @@
+from src.db import read_sql_train, read_sql_test
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+QUERY_TRAIN = "SELECT * FROM application_train"
+QUERY_TEST = "SELECT * FROM application_test"
+
+def load_data_train():
+    return read_sql_train(QUERY_TRAIN)
+
+def load_data_test():
+    return read_sql_test(QUERY_TEST)
+
+def preprocessing(df):
+    df = df.copy()
+    
+    # DAYS_BIRTH -> YEARS
+    df['DAYS_BIRTH_IN_YEAR'] = -df['DAYS_BIRTH'] / 365.25
+    df = df.drop(columns=['DAYS_BIRTH'])
+    
+    # DAYS_EMPLOYED
+    df['IS_UNEMPLOYED'] = (df['DAYS_EMPLOYED'] == 365243).astype(int)
+    df.loc[df['DAYS_EMPLOYED'] == 365243, 'DAYS_EMPLOYED'] = np.nan
+
+    # EMPLOYED_YEARS
+    df['EMPLOYED_YEARS'] = (-df['DAYS_EMPLOYED'] / 365.25).clip(lower=0)
+    df = df.drop(columns=['DAYS_EMPLOYED'])
+
+    # DAYS_ID_PUBLISH -> YEARS
+    df['DAYS_ID_PUBLISH_IN_YEAR'] = -df['DAYS_ID_PUBLISH'] / 365.25
+    df = df.drop(columns=['DAYS_ID_PUBLISH'])
+
+    # CODE_GENDER
+    df['CODE_GENDER'] = df['CODE_GENDER'].replace('XNA', np.nan)
+    
+    # NAME_FAMILY_STATUS
+    df['NAME_FAMILY_STATUS'] = df['NAME_FAMILY_STATUS'].replace('Unknown', np.nan)
+
+    return df
+
+def build_train_test():
+    df_application_train = load_data_train()
+    df_application_test  = load_data_test() 
+
+    X_train = preprocessing(df_application_train.drop(columns=['TARGET']))
+    y_train = df_application_train['TARGET']
+    X_test = preprocessing(df_application_test)
+
+    # проверка размерности признаков после сплит
+    if X_train.shape[1] != X_test.shape[1]:
+        logger.error('Количество признаков после сплит не совпадает, проверьте датасеты')
+    else:
+        logger.info('Проверка размерности признаков после сплит пройдена')
+
+    # проверка имен/состава колонок
+    if set(X_train.columns) != set(X_test.columns):
+        logger.error('Имена признаков после сплит не совпадают, проверьте датасеты')
+    else:
+        logger.info('Проверка имен колонок после сплит пройдена')
