d425fc176c980e785d2856d39d3411253d10afef
John-Gear <plotnikoff.sergey@gmail.com>
2026-02-21 22:22:38 +0300

    train model

--- src/db.py
+++ src/db.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH_TRAIN = BASE_DIR / "data" / "application_train.db"
-DB_PATH_TEST  = BASE_DIR / "data" / "application_test.db"
+DB_PATH_TEST  = BASE_DIR / "data" / "for_predict" / "application_test.db"
 
 def read_sql_train(query: str) -> pd.DataFrame:
     conn = sqlite3.connect(str(DB_PATH_TRAIN))
